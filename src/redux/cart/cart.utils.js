f7bc16fd641eb9853b22d52a721bd1722f5daa8b
Monferd Collin <monferd.j.collin@gmail.com>
2020-03-23 09:44:55 -0400

    fixed error where quantity was not being added properly in utils

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,18 +4,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   )
 
   if (existingCartItem) {
-    console.log('exists so adding 1')
     return (
       cartItems.map(cartItem => {
-        console.log('found a match')
-        if (cartItems.id === cartItemToAdd.id) {
+        if (cartItem.id === cartItemToAdd.id) {
           return { ...cartItem, quantity: cartItem.quantity + 1 }
         }
         else return cartItem
       }
-      ) // fix not adding properly
+      )
     )
   }
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+  else return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
 
