74e5eb9183d2271a1d9ddca2ef51d24e547bee89
Monferd Collin <monferd.j.collin@gmail.com>
2020-03-23 09:39:15 -0400

    added counter functionality in shopping cart icon

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -7,17 +7,22 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div onClick={toggleCartHidden} className='cart-icon'>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{0}</span>
+      <span className='item-count'>{itemCount}</span>
     </div>
   )
 }
+
+const mapState = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0),
+  cart: cartItems
+})
 
 const mapDispatch = dispatch => ({
   toggleCartHidden: async () => await dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDispatch)(CartIcon)
+export default connect(mapState, mapDispatch)(CartIcon)
